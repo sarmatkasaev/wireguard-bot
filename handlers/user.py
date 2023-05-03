72b276637d4641fba9ba5f3bd2ea4e8b6335c99b
Sarmat Kasaev <sarmatkasaev@gmail.com>
2023-05-03 22:20:28 +0300

    link

--- handlers/user.py
+++ handlers/user.py
@@ -35,7 +35,7 @@ async def cmd_start(message: types.Message) -> types.Message:
 
     await message.reply(f"Привет, {message.from_user.full_name or message.from_user.username}!\nЧтобы начать пользоваться VPN, оплати подписку",
                         reply_markup=await kb.free_user_kb(message.from_user.id))
-    await bot.send_message(message.from_user.id, 'Подробное описание бота и его функционала доступно на [странице](https://telegra.ph/FAQ-po-botu-01-08), оплачивая подписку, вы соглашаетесь с правилами использования бота и условиями возврата средств, указанными в статье выше.',
+    await bot.send_message(message.from_user.id, 'Подробное описание бота и его функционала доступно на [странице](https://telegra.ph/FAQ-po-botu-05-03), оплачивая подписку, вы соглашаетесь с правилами использования бота и условиями возврата средств, указанными в статье выше.',
                            parse_mode='Markdown',)
     database.insert_new_user(message)
 
